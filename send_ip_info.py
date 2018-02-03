a4ed2d4f3363ab00f6c844bbd9443bf1ab208fbf
jin.p <arno@arnojin.com>
2018-02-03 20:38:19 +0800

    增加对IP变化的判断。

--- send_ip_info.py
+++ send_ip_info.py
@@ -27,8 +27,8 @@ def get_wan_ip():
     url     = "http://ip.chinaz.com/"
     request = request.urlopen(url).read().decode('utf-8')
     wan_ip  = re.findall(r"<p class=\"getlist pl10\"><span>您来自：</span>(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})", request)[0]
-    log.info(wan_ip)
-    log.info(url)
+    log.info('wan_ip = ' + wan_ip)
+    log.info('url = ' + url)
     log.info("获取外网IP地址 结束 。")
     return [wan_ip, url]
 
@@ -38,7 +38,6 @@ def send_email(sender, receivers, title, content):
 
     import smtplib
     from email.mime.text import MIMEText
-    # from email.header import Header
 
     # 设置邮件服务器信息
     mail_host   = 'mail.tcl.com'        # SMTP服务器
@@ -68,23 +67,48 @@ def send_email(sender, receivers, title, content):
         log.info("发送邮件 失败 ！")
 
 def send_ip_info():
+    import re
+    import time
+    #from datetime import datetime
+
     # 设置发件人、收件人
     sender      = 'username@tcl.com'    # 发件人邮箱（最好写全, 不然会失败）
     receivers   = ['username@tcl.com']  # 接收人邮件列表
 
     # 邮件主题
     wan_ip      = get_wan_ip()
-    title       = '新IP地址：' + wan_ip[0]
+    ip          = wan_ip[0]
+    url         = wan_ip[1]
+    title       = '新IP地址：' + ip
 
-    # 邮件内容
-    content     = "通过 " + wan_ip[1] + " 获取本机外网IP地址\r\n"
-    content     = content + "新IP地址：" + wan_ip[0]
+    # 读取之前保存的ip地址信息
+    # ip_info.txt 文件中默认有一条记录： 2018-02-03 20:30:57 1.2.3.4
+    ip_info_file    = open('ip_info.txt', 'r')
+    ip_info         = ip_info_file.readlines()
+    ip_info_file.close()
+    last_line       = ip_info[-1]
 
-    log.info(sender)
-    log.info(receivers)
-    log.info(title)
-    log.info(content)
-    send_email(sender, receivers, title, content)
+    # 'yyyy-mm-dd hh:mm:ss nnn.nnn.nnn.nnn'
+    info = re.findall(r"(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2})\s(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})", last_line)[0]
+    last_time   = info[0]
+    last_ip     = info[1]
+    log.info('last_time = ' + last_time)
+    log.info('last_ip = ' + last_ip)
+
+    if (last_ip != ip) :
+        # 追加新的ip地址
+        ip_info_file = open('ip_info.txt', 'a')
+        ip_info_file.write('\r' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' ' + ip)
+        ip_info_file.close()
+        
+        # 邮件内容
+        content     = "通过 " + url + " 获取本机外网IP地址\r\n"
+        content     = content + "新IP地址：" +ip
+        log.info('content = ' + content)
+
+        send_email(sender, receivers, title, content)
+    else:
+        log.info("IP地址没有变化。")
 
 # 主程序
 if __name__ == '__main__':

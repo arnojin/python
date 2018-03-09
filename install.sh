bcccd4a4af679b0e1311bfd8e93fa68eb242a783
jin.p <arno@arnojin.com>
2018-03-09 15:57:34 +0800

    更新步骤。

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
 tar -xvzf Python-3.6.4.tgz
 cd Python-3.6.4/
+yum install gcc
 ./configure
 make && make install
 ln -s /usr/local/bin/python3.6 /usr/bin/python3

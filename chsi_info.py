dc74ed630699ca6ea860afc451ccea562877029c
jin.p <arno@arnojin.com>
2018-06-22 10:53:05 +0800

    调整注释格式。

--- chsi_info.py
+++ chsi_info.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-"""
+"""爬取学信网(http://www.chsi.com.cn/)中“高校招生信息服务”板块下的“院校库”、“往年录取分数”
 作者：金鹏
 时间：2018-06-14
 用途：爬取学信网(http://www.chsi.com.cn/)中“高校招生信息服务”板块下的“院校库”、“往年录取分数”
-安装：  pip install lxml
-        pip install bs4
+安装：pip install lxml
+      pip install bs4
 步骤：1、爬取 院校库（http://gaokao.chsi.com.cn/sch/search--ss-on,option-qg,searchType-1,start-0.dhtml）
-        获取 院校名称，院校所在地，院校隶属，院校类型，学历层次，院校特性，研究生院，满意度
+          获取 院校名称，院校所在地，院校隶属，院校类型，学历层次，院校特性，研究生院，满意度
       2、爬取 往年录取分数（http://gaokao.chsi.com.cn/lqfs/）
-        根据 考生来源、院校名称，参考年份，科类
-        获取 录取批次，录取线差，省市分数线，高校平均分，专业平均分（门类、专业名称、平均分），专业录取线差
+          根据 考生来源、院校名称，参考年份，科类
+          获取 录取批次，录取线差，省市分数线，高校平均分，专业平均分（门类、专业名称、平均分），专业录取线差
       3、爬取 各省历年分数线
-        http://gaokao.chsi.com.cn/z/gkbmfslq2014/pcx.jsp
-        http://gaokao.chsi.com.cn/z/gkbmfslq2015/pcx.jsp
-        http://gaokao.chsi.com.cn/z/gkbmfslq2016/pcx.jsp
-        http://gaokao.chsi.com.cn/z/gkbmfslq2017/pcx.jsp
+          http://gaokao.chsi.com.cn/z/gkbmfslq2014/pcx.jsp
+          http://gaokao.chsi.com.cn/z/gkbmfslq2015/pcx.jsp
+          http://gaokao.chsi.com.cn/z/gkbmfslq2016/pcx.jsp
+          http://gaokao.chsi.com.cn/z/gkbmfslq2017/pcx.jsp
 """
 
 import logging
+import time
+
+from urllib import request
+from bs4 import BeautifulSoup
+
 
 logging.basicConfig(filename='./log.log',
-                    format='[%(asctime)s -%(name)s-%(levelname)s-%(funcName)s]%(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S %p',
+                    format='[%(asctime)s] [%(name)s] [%(levelname)s] [%(funcName)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.DEBUG)
 log = logging.getLogger('chsi_info')
 sh = logging.StreamHandler()
 sh.setLevel(level=logging.DEBUG)
-formatter = logging.Formatter('[%(asctime)s -%(name)s-%(levelname)s-%(funcName)s]%(message)s')
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] [%(funcName)s] %(message)s')
 sh.setFormatter(formatter)
 log.addHandler(sh)
 
 
-def get_html(url):
-    """
-    爬取 页面内容
-    """
+def docstring():
+    """文档说明"""
+    return __doc__ % globals()
 
+
+def get_html(url):
+    """爬取 页面内容"""
     log.info("开始 ...")
 
-    from urllib import request
     response_result = request.urlopen(url).read()
     html = response_result.decode('utf-8')
 
@@ -49,13 +55,9 @@ def get_html(url):
 
 
 def get_soup(url):
-    """
-    返回 soup 对象
-    """
-
+    """返回 soup 对象"""
     log.info("开始 ...")
 
-    from bs4 import BeautifulSoup
     html = get_html(url)
     # soup = BeautifulSoup(html, 'html.parser')
     soup = BeautifulSoup(html, 'lxml')
@@ -66,12 +68,8 @@ def get_soup(url):
 
 
 def get_yxk():
-    """
-    获取 院校库 页面内容
-    """
-
+    """获取 院校库 页面内容"""
     log.info("开始 ...")
-    import time
 
     # 第一页地址，每页20个院校，url后缀从0开始，间隔20递增
     # url = 'http://gaokao.chsi.com.cn/sch/search--ss-on,option-qg,searchType-1,start-0.dhtml'
@@ -122,10 +120,7 @@ def get_yxk():
 
 
 def main():
-    """
-    主程序
-    """
-
+    """主程序"""
     log.info("开始 ...")
 
     # 1、爬取 院校库（http://gaokao.chsi.com.cn/sch/search--ss-on,option-qg,searchType-1,start-0.dhtml）
@@ -133,6 +128,7 @@ def main():
     yxk = get_yxk()
     open('yxk-utf8.csv', 'wb').write(yxk.encode('UTF-8'))
     open('yxk-gb18030.csv', 'wb').write(yxk.encode('GB18030'))
+    # print(docstring())
 
     log.info("结束 。")
     return
